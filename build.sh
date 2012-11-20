635556a4b9f039453ef710ce0def9643494d0bac
Alex C <alex3yoyo@gmail.com>
2012-11-19 22:05:20 -0500

    Added args First arg: * -a : build all * -physpro : build only PhysPro * -definition : build only definition Second arg: * -b : make backup of PhysPro-Nspire directory before building

--- build.sh
+++ build.sh
@@ -1,68 +1,79 @@
-#!/bin/sh
+#!/bin/bash
 
 #Mr.Kitty
 
-echo "Backing up..."
+echo "Building $1" 
 
-cd ..
-endTime=$(date +%F_%H.%M.%S)
-backupName="PhysPro-"$endTime".tar.bz2"
-tar -jcvf $backupName "PhysPro-Nspire"
-mv $backupName ./bk/
+if [[ "$2" = -b ]]; then
+    echo "Backing up..."
+    cd ..
+    endTime=$(date +%F_%H.%M.%S)
+    backupName="PhysPro-"$endTime".tar.bz2"
+    tar -jcvf $backupName "PhysPro-Nspire"
+    mv $backupName ./bk/
+    cd ./PhysPro-Nspire/
+    echo "Backup complete"
+else
+    echo "WARNING: A backup was not created."
+fi
 
-cd ./PhysPro-Nspire/
+if [[ "$1" = "-physpro" || "$1" = "-a" ]]; then
+    echo ""
+    echo "Building PhysPro v0.1a..."
+    
+    echo "Building the database"
+    cd 0\ -\ \ Database
+    ./build.sh
+    
+    # echo "Building the analysis"
+    # cd ..
+    # cd 1\ -\ \ Analysis\ Part
+    # ./build.sh
+    
+    echo "Building the FormulaPro core"
+    cd ..
+    cd 2\ -\ \ FormulaPro
+    ./build.sh
+    
+    echo "Building Reference"
+    cd ..
+    cd 3\ -\ \ Reference\ Part
+    ./build.sh
+    
+    echo "Building the libraries"
+    cd ..
+    cd Global\ Libraries
+    ./build.sh
+    cd ..
+    
+    echo "Creating the whole thing..."
+    cat Database.big.lua lib.big.lua FormulaPro.big.lua Reference.big.lua main.lua > PhysPro.big.lua
+    echo "wine luna PhysPro.big.lua PhysPro-Nspire.tns"
+    wine ../../luna-v0.3a/luna.exe PhysPro.big.lua PhysPro-Nspire.tns
+    mv PhysPro-Nspire.tns ./Files/PhysPro-Nspire.tns
+    echo "Done building PhysPro"
 
-echo "Backup complete"
+    echo "Cleaning up"
+    rm lib.big.lua
+    rm FormulaPro.big.lua
+    #rm Analysis.big.lua
+    rm Reference.big.lua
+    rm Database.big.lua
+fi
 
-echo "Building PhysPro v0.1a..."
-echo "Building the database"
+if [[ "$1" = "-definitions" || "$1" = "-a" ]]; then
+    echo ""
+    
+    echo "Building definitions"
+    cd ./definitions
+    ./build.sh
+    cd ..
+    echo "wine luna definitions.big.lua definitions.tns"
+    wine ../../luna-v0.3a/luna.exe definitions.big.lua definitions.tns
+    mv definitions.tns ./Files/definitions.tns
+    echo "Done building definitions"
+fi
 
-cd 0\ -\ \ Database
-./build.sh
-
-#cd ..
-#cd 1\ -\ \ Analysis\ Part
-#./build.sh
-
-echo "Building the FormulaPro core"
-cd ..
-cd 2\ -\ \ FormulaPro
-./build.sh
-
-echo "Building Reference"
-cd ..
-cd 3\ -\ \ Reference\ Part
-./build.sh
-
-echo "Building the libraries"
-cd ..
-cd Global\ Libraries
-./build.sh
-
-cd ..
-
-echo "Creating the whole thing..."
-cat Database.big.lua lib.big.lua FormulaPro.big.lua Reference.big.lua main.lua > PhysPro.big.lua
-echo "wine luna PhysPro.big.lua PhysPro-Nspire.tns"
-wine ../../luna-v0.3a/luna.exe PhysPro.big.lua PhysPro-Nspire.tns
-mv PhysPro-Nspire.tns ./Files/PhysPro-Nspire.tns
-echo "Done building PhysPro"
-
-echo "Building definitions"
-cd ./definitions
-./build.sh
-cd ..
-echo "wine luna definitions.big.lua definitions.tns"
-wine ../../luna-v0.3a/luna.exe definitions.big.lua definitions.tns
-mv definitions.tns ./Files/definitions.tns
-echo "Done building definitions"
-
-echo "Cleaning up"
-rm lib.big.lua
-rm FormulaPro.big.lua
-#rm Analysis.big.lua
-rm Reference.big.lua
-rm Database.big.lua
-
+echo ""
 echo "Done. Enjoy!"
 
