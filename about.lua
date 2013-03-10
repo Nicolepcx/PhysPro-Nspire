2ac0b290b95946fbcead8345baa0dbd8ec7244a6
Alex Coll <alex3yoyo@gmail.com>
2013-03-10 15:02:16 -0400

    Added topic 6 formulas and units

--- about.lua
+++ about.lua
@@ -6,8 +6,8 @@
 
 0-------------------0
 |                   |
-|   PhysPro v1.2.0  |
-|   (25 Feb 2013)   |
+|   PhysPro v1.2.1  |
+|   (10 Mar 2013)   |
 |   LGLP 3 License  |
 |     alex3yoyo     |
 |                   |
@@ -23,7 +23,7 @@ TI-Planet.org       Inspired-Lua.org
 pInfo = {
     name="PhysPro-Nspire",
     by="alex3yoyo",
-    ver="v1.2.0",
+    ver="v1.2.1",
     web="http://github.com/alex3yoyo/physpro-nspire",
     license="LGPL3 License"
 }
