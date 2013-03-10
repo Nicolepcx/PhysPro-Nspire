f2f2dbfad9b681299999b18b3cdbe3f8a05ad157
Alex Coll <alex3yoyo@gmail.com>
2013-03-10 15:13:50 -0400

    more build script modifications

--- build.sh
+++ build.sh
@@ -34,7 +34,7 @@ if [[ "$ARGS" == *"-luna"* ]]; then
     cp PhysPro.src.lua PhysPro.src.lua.tns
     echo "Done building PhysPro"
 elif [[ "$ARGS" == *"-etk"* ]]; then
-    build/etk main.lua build/PhysPro-Nspire.tns
+    build/etk.lua main.lua build/PhysPro-Nspire.tns
     cp build/PhysPro.src.lua build/PhysPro.src.lua.tns
 fi
 
